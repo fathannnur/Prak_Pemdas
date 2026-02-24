1e59703802ee2922afe3138460ec2a55abb25313
fathannnur <carelius02@gmail.com>
2026-02-24 19:51:34 +0700

    Menambahkan namespace

--- file1.cpp
+++ file1.cpp
@@ -46,3 +46,14 @@ int main(){
     cout << umur;
 }
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int umur = 15;
+    umur = 20;
+
+    cout <<"ini string" << endl;
+    cout << 123 << endl;
+    cout << umur;
+}
